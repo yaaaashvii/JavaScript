2c86bcc34951c334ef1d482fec395d31eac75e2a
Yashvi Gupta <119882714+yaaaashvii@users.noreply.github.com>
2024-03-01 21:17:09 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 144556
+let accountEmail = "yashvi@gmail.com"
+var  accountPassword = "12345"
+accountCity = "Lucknow"
+let accountState;
+
+//let doesnot have scope problem but nowdays var is not used now bcz of block and functional scope prblm 
+//prefer to use let only 
+
+//accountId = 2 //not allowed 
+accountEmail= "hc@gmail.com"
+accountPassword ="212121"
+accountCity= "kanpur"
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
+// undefined output when no value is asigned to any variable
+
+
+ 
