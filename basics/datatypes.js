c11da151e659eb3543e0fccc7310f11d5fb2ee58
Yashvi Gupta <119882714+yaaaashvii@users.noreply.github.com>
2024-03-02 13:36:46 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all js code as newer version
+
+//alert("hello")
+//alert(3+3) //we are using node js not browser here so it will throw an error
+
+console.log(3+3)
+console.log("yashvi") 
+
+let name = "yashvi"
+let age = 20
+let isLoggedIn = false 
+
+
+// number => 2^53 range 
+//bigint // not used usually 
+//string can be used in => '' or ""
+//boolean => t/f
+//null (stand alone value) (represtation of empty value)
+//undefined (when no value is assigned)
+//symbol  => unique
+//object 
+
+console.log(typeof "yashvi");
+console.log(typeof age);
+console.log(typeof null);  //o/p undefined bcz its a type 
+console.log(typeof undefined);  //o/p object 
+
+
+
